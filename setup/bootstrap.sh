0dc104feea485031240aebab10398f7702ddd602
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-10-20 11:53:40 -0400

    initial-commit

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -52,11 +52,11 @@ docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com pe
 
 # Wait for Hyperledger Fabric to start
 # In case of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
-export FABRIC_START_TIMEOUT=10
+export FABRIC_START_TIMEOUT=15
 #echo ${FABRIC_START_TIMEOUT}
 sleep ${FABRIC_START_TIMEOUT}
 
-# Create the channel
+# Create the application channel
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/$CHANNEL_NAME.tx
 # Join peer0.org1.example.com to the channel
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b $CHANNEL_NAME.block
--- setup/startup.sh
+++ setup/startup.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+#!/bin/sh
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
 cd ~/Desktop
 curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.1 1.4.9
@@ -7,13 +12,4 @@ git clone https://github.com/rolivieri/lfs272-assets.git
 sleep 15
 cd lfs272-assets/networkFiles
 mkdir config
-wget https://s3.us-east-2.amazonaws.com/lfx-start1/bootstrap.sh
-chmod u+x ./bootstrap.sh 
-chmod u+x start.sh teardown.sh stop.sh 
-./bootstrap.sh
-
-sleep 30s 
-
-cd ..
-rm lfs272-assets/networkFiles/bootstrap.sh ./startup.sh
-rm -- "$0"
+wget -O https://raw.githubusercontent.com/rolivieri/lfs272-assets/main/setup/bootstrap.sh | bash
