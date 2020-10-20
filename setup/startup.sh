a33d9dc14cec2304f6158a6e8e31b573629ebff8
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-10-20 14:48:53 -0400

    initial-commit

--- setup/startup.sh
+++ setup/startup.sh
@@ -10,6 +10,6 @@ curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.1 1.4.9
 sleep 15
 git clone https://github.com/rolivieri/lfs272-assets.git
 rm -rf lfs272-assets/setup
-cd lfs272-assets/networkFiles
-echo "Bootstrapping starter network..."
-wget -O - https://raw.githubusercontent.com/rolivieri/lfs272-assets/main/setup/bootstrap.sh | bash
+#cd lfs272-assets/networkFiles
+#echo "Bootstrapping starter network..."
+#wget -O - https://raw.githubusercontent.com/rolivieri/lfs272-assets/main/setup/bootstrap.sh | bash
