e02229b857780d1357d0df9b7295a45948dc0c9e
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-10-20 11:55:20 -0400

    initial-commit

--- setup/startup.sh
+++ setup/startup.sh
@@ -9,7 +9,7 @@ cd ~/Desktop
 curl -sSL https://bit.ly/2ysbOFE | bash -s -- 2.2.1 1.4.9
 sleep 15
 git clone https://github.com/rolivieri/lfs272-assets.git
-sleep 15
+rm -rf lfs272-assets/setup
 cd lfs272-assets/networkFiles
 mkdir config
 wget -O https://raw.githubusercontent.com/rolivieri/lfs272-assets/main/setup/bootstrap.sh | bash
