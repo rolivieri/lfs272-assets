7cffe367a91dd22aedc45b030f04d7e047cee079
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-10-22 13:09:26 -0400

    initial-commit

--- networkFiles/teardown.sh
+++ networkFiles/teardown.sh
@@ -11,6 +11,7 @@ set -e
 docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.yml down
 
 # Remove chaincode Docker images
+# https://github.com/hyperledger/fabric-samples/blob/master/test-network/network.sh#L25
 #docker ps --filter name=NAMEHERE* -aq | xargs docker stop | xargs docker rm
 #docker rmi $(docker images dev-* -q)
 
