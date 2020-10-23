b8094331930faa20324283c5088f6ed4d6e10216
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-10-23 19:39:19 -0400

    initial commit

--- networkFiles/start.sh
+++ networkFiles/start.sh
@@ -12,7 +12,7 @@ export CHANNEL_NAME=allarewelcome
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.org1.example.com orderer.example.com peer0.org1.example.com
+docker-compose -f docker-compose.yml up -d ca.org1.example.com orderer.example.com peer0.org1.example.com cli
 
 # Wait for Hyperledger Fabric to start
 # In case of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
@@ -20,6 +20,6 @@ export FABRIC_START_TIMEOUT=15
 sleep ${FABRIC_START_TIMEOUT}
 
 # Create the application channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/$CHANNEL_NAME.tx
+docker exec cli peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/$CHANNEL_NAME.tx --tls --cafile $ORDERER_TLS_CA
 # Join peer0.org1.example.com to the channel
-docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/fabric/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b $CHANNEL_NAME.block
+docker exec cli peer channel join -b $CHANNEL_NAME.block
