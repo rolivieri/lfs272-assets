c383a38ea23f6b5758d012e4bfe059d089c113e1
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-10-23 09:21:17 -0400

    initial-commit

--- networkFiles/teardown.sh
+++ networkFiles/teardown.sh
@@ -13,14 +13,14 @@ docker-compose -f docker-compose.yml kill && docker-compose -f docker-compose.ym
 # Remove chaincode Docker container and images
 CONTAINER_IDS=$(docker ps -a | awk '($2 ~ /dev-peer.*/) {print $1}')
 if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
-    echo "No containers available for deletion"
+    echo "No containers available for deletion."
 else
     docker rm -f $CONTAINER_IDS
 fi
 
 DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*/) {print $3}')
 if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
-    echo "No images available for deletion"
+    echo "No images available for deletion."
 else
     docker rmi -f $DOCKER_IMAGE_IDS
 fi
