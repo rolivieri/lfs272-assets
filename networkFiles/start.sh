163770299d8815477cdd50533433f8370397a664
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-10-23 19:52:01 -0400

    initial commit

--- networkFiles/start.sh
+++ networkFiles/start.sh
@@ -20,6 +20,7 @@ export FABRIC_START_TIMEOUT=15
 sleep ${FABRIC_START_TIMEOUT}
 
 # Create the application channel
+ORDERER_TLS_CA=`docker exec cli  env | grep ORDERER_TLS_CA | cut -d'=' -f2`
 docker exec cli peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f /etc/hyperledger/configtx/$CHANNEL_NAME.tx --tls --cafile $ORDERER_TLS_CA
 # Join peer0.org1.example.com to the channel
 docker exec cli peer channel join -b $CHANNEL_NAME.block
