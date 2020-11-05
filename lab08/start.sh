d91a819d48ed5d02c020468163f89f6cac72969e
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-11-05 12:49:16 -0500

    initial commit

--- lab08/start.sh
+++ lab08/start.sh
@@ -9,7 +9,10 @@ set -ev
 
 docker-compose -f docker-compose.yml down
 
-docker-compose -f docker-compose.yml up -d ca.org1.example.com orderer.example.com couchdbOrg1Peer0 peer0.org1.example.com couchdbOrg1Peer1 peer1.org1.example.com cli
+docker-compose -f docker-compose.yml up -d orderer.example.com couchdbOrg1Peer0 peer0.org1.example.com \
+    couchdbOrg1Peer1 peer1.org1.example.com ca.org1.example.com \
+    couchdbOrg2Peer0 peer0.org2.example.com couchdbOrg2Peer1 peer1.org2.example.com ca.org2.example.com \
+    cli
 
 # Wait for Hyperledger Fabric to start
 # In case of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
