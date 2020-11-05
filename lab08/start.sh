26b778445bc9ebda2016b9f925941c408405c16c
Ricardo Olivieri <r_olivieri@yahoo.com>
2020-11-05 12:35:16 -0500

    initial commit

---
+++ lab08/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Copyright IBM Corp All Rights Reserved
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+set -ev
+
+docker-compose -f docker-compose.yml down
+
+docker-compose -f docker-compose.yml up -d ca.org1.example.com orderer.example.com couchdbOrg1Peer0 peer0.org1.example.com couchdbOrg1Peer1 peer1.org1.example.com cli
+
+# Wait for Hyperledger Fabric to start
+# In case of errors when running later commands, issue export FABRIC_START_TIMEOUT=<larger number>
+export FABRIC_START_TIMEOUT=15
+sleep ${FABRIC_START_TIMEOUT}
